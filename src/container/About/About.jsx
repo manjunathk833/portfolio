5631d19c01db2baa3d6f212141ddf80b3dbee600
manjunath h k <manjunathhk833@gmail.com>
2022-03-20 21:27:35 +0530

    completed all sections needs to be customised for my profile

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,33 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import {motion} from 'framer-motion';
 
-import {images} from '../../constants'
-import './About.scss';
+import { AppWrap, MotionWrap } from '../../wrapper';
 
-const abouts = [
-    {
-      title: 'Web Development',
-      description: 'I am a good web developer',
-      imgUrl: images.about01
-    },
-    {
-      title: 'Automation Engineer',
-      description: 'I am a good Automation Engineer',
-      imgUrl: images.about02
-    },
-    {
-      title: 'UI/UX',
-      description: 'I am a good UI/UX designer',
-      imgUrl: images.about03
-    },
-    {
-      title: 'Web Development',
-      description: 'I am a good back end developer',
-      imgUrl: images.about04
-    }
-];
+import './About.scss';
+import { urlFor, client } from '../../client';
+
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+      client.fetch(query)
+      .then((data) =>{
+        setAbouts(data)
+      })
+  }, []);
+  
+
   return (
     <>
       <h2 className='head-text'>I Know that <span>Good Design</span><br /> means <span>Good Business</span></h2>
@@ -41,15 +32,18 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
             >
-             <img src={about.imgUrl} alt={about.title}/> 
+             <img src={urlFor(about.imgUrl)} alt={about.title}/> 
              <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
              <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
           </motion.div>
         ))}
-
       </div>
     </>
   )
 }
 
-export default About
+export default AppWrap(
+  MotionWrap(About, 'app__about'), 
+  'about',
+  "app__whitebg"
+  );
